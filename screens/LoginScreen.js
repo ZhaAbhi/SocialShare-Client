edcf12cc9a3480eac7f2e9eecc47521c40f0feee
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-05-18 18:47:48 +0545

    Extracted inline styling to stylesheet object

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,43 +6,20 @@ import {
   TextInput,
   TouchableWithoutFeedback,
   Keyboard,
+  Image,
+  StyleSheet,
 } from 'react-native';
+import GithubLogo from '../assets/images/Github-logo.png';
 
 const LoginScreen = () => {
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <SafeAreaView>
-        <View
-          style={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: '10%',
-          }}>
-          <Text
-            style={{
-              fontFamily: 'Poppins-SemiBold',
-              color: 'darkblue',
-              fontSize: 28,
-              fontWeight: 'bold',
-            }}>
-            Login here
-          </Text>
+        <View style={styles.headlineContainer}>
+          <Text style={styles.loginText}>Login here</Text>
           <View style={{marginTop: '5%'}}>
-            <Text
-              style={{
-                fontFamily: 'Poppins-SemiBold',
-                color: '#000',
-                fontSize: 17,
-              }}>
-              Welcome back you've
-            </Text>
-            <Text
-              style={{
-                textAlign: 'center',
-                fontFamily: 'Poppins-SemiBold',
-                color: '#000',
-                fontSize: 17,
-              }}>
+            <Text style={styles.welcomeText}>Welcome back you've</Text>
+            <Text style={[styles.welcomeText, {textAlign: 'center'}]}>
               been missed!
             </Text>
           </View>
@@ -50,79 +27,116 @@ const LoginScreen = () => {
         <View style={{padding: 20, marginTop: 30}}>
           <TextInput
             placeholder="Email"
-            style={{
-              height: 40,
-              backgroundColor: '#f1f4ff',
-              borderRadius: 8,
-              marginBottom: 28,
-              paddingLeft: 10,
-            }}
+            style={[styles.textInput, {marginBottom: 28}]}
           />
-          <TextInput
-            placeholder="Password"
-            style={{
-              height: 40,
-              backgroundColor: '#f1f4ff',
-              borderRadius: 8,
-              paddingLeft: 10,
-            }}
-          />
-          <Text
-            style={{
-              marginLeft: 'auto',
-              marginTop: 35,
-              fontFamily: 'Poppins-SemiBold',
-              color: 'darkblue',
-            }}>
-            Forget your password?
-          </Text>
-          <View
-            style={{
-              shadowColor: 'darkblue',
-              shadowOffset: {
-                width: 0,
-                height: 9,
-              },
-              shadowOpacity: 0.5,
-              shadowRadius: 12.35,
-
-              elevation: 19,
-              backgroundColor: 'darkblue',
-              padding: 13,
-              borderRadius: 8,
-              alignItems: 'center',
-              marginTop: 20,
-            }}>
-            <Text
-              style={{
-                color: '#fff',
-                fontFamily: 'Poppins-SemiBold',
-                fontSize: 15,
-              }}>
-              Sign in
-            </Text>
+          <TextInput placeholder="Password" style={styles.textInput} />
+          <Text style={styles.forgotPasswordText}>Forget your password?</Text>
+          <View style={styles.signInButtonContainer}>
+            <Text style={styles.signinText}>Sign in</Text>
           </View>
-          <View
-            style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginTop: 30,
-            }}>
-            <Text style={{fontFamily: 'Poppins-SemiBold', color: '#000'}}>
-              Create a new account
-            </Text>
-            <Text
-              style={{
-                marginTop: 30,
-                fontFamily: 'Poppins-SemiBold',
-                color: 'darkblue',
-              }}>
-              or continue with
-            </Text>
+          <View style={styles.createAccount}>
+            <Text style={styles.createAccountText}>Create a new account</Text>
+            <View style={styles.createAccountContainer}>
+              <Text style={styles.continueText}>or continue with</Text>
+              <View style={styles.githubLoginContainer}>
+                <Image source={GithubLogo} style={styles.githublogo} />
+                <Text style={styles.githubText}>GitHub</Text>
+              </View>
+            </View>
           </View>
         </View>
       </SafeAreaView>
     </TouchableWithoutFeedback>
   );
 };
+
+const styles = StyleSheet.create({
+  headlineContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '10%',
+  },
+  loginText: {
+    fontFamily: 'Poppins-SemiBold',
+    color: 'darkblue',
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+  welcomeText: {
+    fontFamily: 'Poppins-SemiBold',
+    color: '#000',
+    fontSize: 17,
+  },
+  textInput: {
+    height: 40,
+    backgroundColor: '#f1f4ff',
+    borderRadius: 8,
+    paddingLeft: 10,
+  },
+  forgotPasswordText: {
+    marginLeft: 'auto',
+    marginTop: 35,
+    fontFamily: 'Poppins-SemiBold',
+    color: 'darkblue',
+  },
+  signInButtonContainer: {
+    shadowColor: 'darkblue',
+    shadowOffset: {
+      width: 0,
+      height: 9,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 12.35,
+
+    elevation: 19,
+    backgroundColor: 'darkblue',
+    padding: 13,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  signinText: {
+    color: '#fff',
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 15,
+  },
+  createAccountContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  continueText: {
+    fontFamily: 'Poppins-SemiBold',
+    color: 'darkblue',
+  },
+  createAccount: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  createAccountText: {
+    fontFamily: 'Poppins-SemiBold',
+    color: '#000',
+  },
+  githubLoginContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#000',
+    marginLeft: 10,
+    borderRadius: 5,
+  },
+  githublogo: {
+    height: 40,
+    width: 40,
+    borderRadius: 25,
+  },
+  githubText: {
+    color: '#000',
+    fontWeight: 'bold',
+    marginRight: 4,
+  },
+});
 export default LoginScreen;
