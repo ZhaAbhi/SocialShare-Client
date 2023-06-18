835a6fc0a0371167431585f34afada80ee1a794c
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-06-18 17:58:33 +0545

    Styled post content screen

--- screens/PostContentScreen.js
+++ screens/PostContentScreen.js
@@ -7,83 +7,141 @@ import {
   StyleSheet,
   TextInput,
   Image,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from 'react-native';
 import {colors} from '../config/colors';
 import loadingImage from '../assets/images/loadingImage.jpeg';
+import CameraIcon from 'react-native-vector-icons/Entypo';
 
 const PostContentScreen = ({navigation}) => {
   return (
-    <View>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          padding: 20,
-          borderBottomWidth: StyleSheet.hairlineWidth,
-        }}>
-        <Pressable onPress={() => navigation.goBack()}>
-          <Text style={{fontSize: 20, color: '#000'}}>X</Text>
-        </Pressable>
-        <TouchableOpacity
-          activeOpacity={0.8}
-          style={{
-            backgroundColor: colors.primary,
-            paddingLeft: 15,
-            paddingRight: 15,
-            paddingTop: 2,
-            paddingBottom: 2,
-            borderRadius: 20,
-          }}>
-          <Text
-            style={{
-              fontSize: 15,
-              color: '#fff',
-              fontFamily: 'Poppins-semiBold',
-            }}>
-            Post
-          </Text>
-        </TouchableOpacity>
-      </View>
-      <View style={{padding: 10}}>
-        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-          <Image
-            source={loadingImage}
-            style={{
-              height: 30,
-              width: 30,
-              borderRadius: 15,
-              resizeMode: 'cover',
-              marginRight: 5,
-            }}
-          />
+    <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 30 : 0}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={{flex: 1, justifyContent: 'space-between', padding: 20}}>
+          <View>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+              }}>
+              <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Text style={{fontSize: 17}}>X</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={{
+                  paddingLeft: 15,
+                  paddingRight: 15,
+                  paddingTop: 2,
+                  paddingBottom: 2,
+                  backgroundColor: colors.primary,
+                  borderRadius: 20,
+                }}>
+                <Text
+                  style={{
+                    fontSize: 15,
+                    color: '#fff',
+                    fontFamily: 'Poppins-semiBold',
+                  }}>
+                  Post
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                borderBottomWidth: StyleSheet.hairlineWidth,
+                marginTop: 7,
+              }}></View>
+            <View style={{marginTop: 20}}>
+              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Image
+                  source={loadingImage}
+                  style={{height: 30, width: 30, borderRadius: 15}}
+                />
+                <View
+                  style={{
+                    paddingLeft: 5,
+                    paddingRight: 5,
+                    backgroundColor: colors.primary,
+                    marginLeft: 5,
+                    borderRadius: 5,
+                  }}>
+                  <Text style={{color: '#fff', fontFamily: 'Poppins-semiBold'}}>
+                    Public
+                  </Text>
+                </View>
+              </View>
+              <View style={{marginTop: 10, marginLeft: 15}}>
+                <TextInput
+                  autoFocus={true}
+                  textAlignVertical="Top"
+                  multiline={true}
+                  maxLength={300}
+                  style={{padding: 5}}
+                />
+              </View>
+            </View>
+          </View>
           <View
             style={{
-              backgroundColor: colors.primary,
-              paddingRight: 5,
-              paddingLeft: 5,
-              borderRadius: 2,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
             }}>
-            <Text
+            <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity
+                style={{
+                  height: 80,
+                  width: 80,
+                  backgroundColor: 'lightgrey',
+                  borderRadius: 10,
+                  marginRight: 8,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <CameraIcon name="camera" size={50} color="#000" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  height: 80,
+                  width: 80,
+                  backgroundColor: 'grey',
+                  borderRadius: 10,
+                }}>
+                <Image
+                  source={loadingImage}
+                  style={{
+                    height: 80,
+                    width: 80,
+                    borderRadius: 10,
+                    position: 'absolute',
+                    resizeMode: 'cover',
+                  }}
+                />
+              </TouchableOpacity>
+            </View>
+            <View
               style={{
-                color: '#fff',
-                fontFamily: 'Poppins-semiBold',
-                fontSize: 13,
+                height: 35,
+                width: 35,
+                borderWidth: 1,
+                borderColor: 'red',
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 18,
               }}>
-              Public
-            </Text>
+              <Text>200</Text>
+            </View>
           </View>
         </View>
-        <View style={{marginLeft: 15}}>
-          <TextInput
-            textAlignVertical="top"
-            multiline={true}
-            autoFocus={true}
-            style={{height: '70%', padding: 5}}
-          />
-        </View>
-      </View>
-    </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
