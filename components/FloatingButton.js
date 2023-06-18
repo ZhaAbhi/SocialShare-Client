47ae89a9f14e1b5051655d15fdfd7558d7486a50
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-06-18 21:39:54 +0545

    Seperated styles of floating button

--- components/FloatingButton.js
+++ components/FloatingButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import {Text, TouchableOpacity, StyleSheet} from 'react-native';
 import {colors} from '../config/colors';
 
 const FloatingButton = ({onPress}) => {
@@ -7,20 +7,24 @@ const FloatingButton = ({onPress}) => {
     <TouchableOpacity
       onPress={onPress}
       activeOpacity={0.8}
-      style={{
-        height: 50,
-        width: 50,
-        borderRadius: 35,
-        backgroundColor: colors.primary,
-        position: 'absolute',
-        right: 15,
-        bottom: 15,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
+      style={styles.container}>
       <Text style={{fontSize: 30, color: '#fff'}}>+</Text>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: 50,
+    width: 50,
+    borderRadius: 35,
+    backgroundColor: colors.primary,
+    position: 'absolute',
+    right: 15,
+    bottom: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default FloatingButton;
