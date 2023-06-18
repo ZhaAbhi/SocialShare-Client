8af427d0d9f9ba2572df435b9871c785415279db
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-06-18 21:41:41 +0545

    Added shadow on floating button

--- components/FloatingButton.js
+++ components/FloatingButton.js
@@ -18,12 +18,22 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     borderRadius: 35,
+    opacity: 0.7,
     backgroundColor: colors.primary,
     position: 'absolute',
     right: 15,
     bottom: 15,
     justifyContent: 'center',
     alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+
+    elevation: 8,
   },
 });
 
