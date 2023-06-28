d6833c7209de163a04f5c7255eefe25293cc8d85
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-06-28 21:21:55 +0545

    Cleaned all files to restart building again from scratch

--- App.tsx
+++ App.tsx
@@ -1,23 +1,11 @@
-import React, {useState} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import AuthenticationNavigator from './navigation/AuthenticationNavigator';
-import AuthContext from './context/AuthContext';
-import UnAuthenticatedHeader from './components/UnAuthenticatedHeader';
-import UnAuthenticationNavigator from './navigation/UnAuthenticationNavigator';
-import UserContext from './context/UserContext';
-import DrawerNavigator from './navigation/DrawerNavigator';
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
 
 const App = () => {
-  const [loggedIn, setLoggedIn] = useState();
-  const [user, setUser] = useState();
   return (
-    <NavigationContainer>
-      <AuthContext.Provider value={{loggedIn, setLoggedIn}}>
-        <UserContext.Provider value={{user, setUser}}>
-          {loggedIn ? <DrawerNavigator /> : <UnAuthenticationNavigator />}
-        </UserContext.Provider>
-      </AuthContext.Provider>
-    </NavigationContainer>
+    <SafeAreaView>
+      <Text>Hello world</Text>
+    </SafeAreaView>
   );
 };
 
