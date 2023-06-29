f145e3b3fa5a6b5776d1c560ea0e5e4a310cbe99
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-06-29 11:06:32 +0545

    Screens for authentication

---
+++ screens/RegisterScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const RegisterScreen = () => {
+  return (
+    <View>
+      <Text>This is register screen</Text>
+    </View>
+  );
+};
+export default RegisterScreen;
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const WelcomeScreen = () => {
+  return (
+    <View>
+      <Text>This is welcome screen</Text>
+    </View>
+  );
+};
+export default WelcomeScreen;
