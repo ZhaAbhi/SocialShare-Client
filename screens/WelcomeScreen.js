f70fcc1201ef00876a1de9139f310d180a6c47e4
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-06-30 22:59:14 +0545

    Completed design of register screen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -9,9 +9,15 @@ const WelcomeScreen = () => {
     <LinearGradient
       colors={['#b7e4c7', '#52b788', '#40916c']}
       style={{flex: 1}}>
-      <SafeAreaView style={{flex: 1, alignItems: 'center'}}>
+      <SafeAreaView style={{flex: 1}}>
         <Image source={WelcomeScreenPng} style={{height: 300, width: '100%'}} />
-        <Text style={{color: '#fff', fontFamily: 'Poppins-Bold', fontSize: 35}}>
+        <Text
+          style={{
+            color: '#fff',
+            fontFamily: 'Poppins-Bold',
+            fontSize: 35,
+            textAlign: 'center',
+          }}>
           Social Share
         </Text>
         <View
@@ -39,42 +45,77 @@ const WelcomeScreen = () => {
             Join our social media revolution
           </Text>
         </View>
-        <View
-          style={{
-            marginTop: '15%',
-            width: '100%',
-            marginVertical: 20,
-            shadowColor: '#000',
-            shadowOffset: {
-              width: 0,
-              height: 7,
-            },
-            shadowOpacity: 0.43,
-            shadowRadius: 9.51,
-
-            elevation: 15,
-          }}>
-          <LinearGradient
-            end={{x: 0, y: 0}}
-            start={{x: 1, y: 0}}
-            colors={['#2d6a4f', '#1b4332']}
+        <View style={{marginTop: '10%', padding: 20}}>
+          <View
             style={{
-              backgroundColor: 'red',
-              alignItems: 'center',
-              height: 35,
-              justifyContent: 'center',
-              borderRadius: 25,
+              marginBottom: 15,
+              width: '100%',
+              shadowColor: '#000',
+              shadowOffset: {
+                width: 0,
+                height: 7,
+              },
+              shadowOpacity: 0.43,
+              shadowRadius: 9.51,
+
+              elevation: 15,
             }}>
-            <Text
+            <LinearGradient
+              end={{x: 0, y: 0}}
+              start={{x: 1, y: 0}}
+              colors={['#2d6a4f', '#1b4332']}
               style={{
-                color: '#fff',
-                fontFamily: 'Poppins-SemiBold',
-                fontSize: 18,
-                letterSpacing: 0.2,
+                backgroundColor: 'red',
+                alignItems: 'center',
+                height: 40,
+                justifyContent: 'center',
+                borderRadius: 25,
               }}>
-              LOGIN
-            </Text>
-          </LinearGradient>
+              <Text
+                style={{
+                  color: '#fff',
+                  fontFamily: 'Poppins-SemiBold',
+                  fontSize: 18,
+                  letterSpacing: 0.2,
+                }}>
+                LOGIN
+              </Text>
+            </LinearGradient>
+          </View>
+          <View
+            style={{
+              width: '100%',
+              shadowColor: '#000',
+              shadowOffset: {
+                width: 0,
+                height: 7,
+              },
+              shadowOpacity: 0.43,
+              shadowRadius: 9.51,
+              elevation: 15,
+            }}>
+            <LinearGradient
+              end={{x: 0, y: 0}}
+              start={{x: 1, y: 0}}
+              colors={['#40916c', '#40916c']}
+              style={{
+                borderWidth: 1,
+                alignItems: 'center',
+                height: 40,
+                justifyContent: 'center',
+                borderRadius: 25,
+              }}>
+              <Text
+                style={{
+                  color: '#fff',
+                  fontFamily: 'Poppins-SemiBold',
+                  fontSize: 18,
+                  letterSpacing: 0.2,
+                }}>
+                REGISTER
+              </Text>
+            </LinearGradient>
+          </View>
         </View>
       </SafeAreaView>
     </LinearGradient>
