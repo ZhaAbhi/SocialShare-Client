9507be605fecb7efae03e87900abf30f2c2204eb
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-06-30 21:54:46 +0545

    Linear gradient background color in welcome screen and added greetings

--- App.tsx
+++ App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import WelcomeScreen from './screens/WelcomeScreen';
 
 const App = () => {
-  return (
-    <SafeAreaView>
-      <Text>Hello world</Text>
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 };
 
 export default App;
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, SafeAreaView} from 'react-native';
+import WelcomeScreenPng from '../assets/images/welcomeScreen.png';
+import LinearGradient from 'react-native-linear-gradient';
 
 const WelcomeScreen = () => {
   return (
-    <View>
-      <Text>This is welcome screen</Text>
-    </View>
+    <LinearGradient
+      colors={['#b7e4c7', '#52b788', '#40916c']}
+      style={{flex: 1}}>
+      <SafeAreaView style={{flex: 1, alignItems: 'center'}}>
+        <Image source={WelcomeScreenPng} style={{height: 300, width: '100%'}} />
+        <Text style={{color: '#fff', fontFamily: 'Poppins-Bold', fontSize: 35}}>
+          Social Share
+        </Text>
+        <View style={{justifyContent:'center', alignItems:'center', padding:5}}>
+          <Text style={{fontSize: 17, fontFamily:'Poppins-SemiBold', color:'#fff'}}>
+          Join our social media revolution.
+          </Text>
+          <Text style={{textAlign:'center',fontSize: 17, fontFamily:'Poppins-SemiBold', color:'#fff'}}>Discover a world of endless connections</Text>
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 export default WelcomeScreen;
