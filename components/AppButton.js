86d3238f25fbfbd614a208deb35d4a33b7f2f487
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-07-01 12:35:28 +0545

    Removed shadow from button

--- components/AppButton.js
+++ components/AppButton.js
@@ -22,18 +22,8 @@ const styles = StyleSheet.create({
   buttonContainer: {
     marginBottom: 15,
     width: '100%',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 7,
-    },
-    shadowOpacity: 0.43,
-    shadowRadius: 9.51,
-
-    elevation: 15,
   },
   buttonBackground: {
-    backgroundColor: 'red',
     alignItems: 'center',
     height: 40,
     justifyContent: 'center',
