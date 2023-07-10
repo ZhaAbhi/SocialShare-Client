d6709282f7ec3ab676b301900f3f2db29a609a17
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-07-10 15:05:08 +0545

    UnAuth UI for login and register screen

---
+++ components/AppUnAuth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const AppUnAuth = () => {
+  return (
+    <View>
+      <Text>This is reusable login and register screen ui</Text>
+    </View>
+  );
+};
+export default AppUnAuth;
