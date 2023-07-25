2c14580921aef77854417fc7b6d464a5f03ab15b
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-07-25 14:05:40 +0545

    Added post card component

---
+++ components/PostCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, TouchableOpacity, Text, Image} from 'react-native';
+
+const PostCard = () => {
+  return (
+    <View>
+      <Text>This is post card</Text>
+    </View>
+  );
+};
+export default PostCard;
