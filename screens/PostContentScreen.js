9bca77058e81eb1d5875ee500175d6551263dcf2
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-08-13 16:25:38 +0545

    Completed the design of post content screen

--- screens/PostContentScreen.js
+++ screens/PostContentScreen.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {
+  Alert,
   Image,
   Keyboard,
   KeyboardAvoidingView,
@@ -15,9 +16,28 @@ import {
 import {colors} from '../utils/colors';
 import loadingImage from '../assets/images/loadingImage.jpeg';
 import CameraIcon from 'react-native-vector-icons/Entypo';
+import {launchImageLibrary} from 'react-native-image-picker';
 
 const PostContentScreen = ({navigation}) => {
   const [textContent, setTextContent] = useState('');
+  const [imagePicked, setImagePicked] = useState(null);
+
+  const pickImage = async () => {
+    await launchImageLibrary({
+      mediaType: 'photo',
+      selectionLimit: 1,
+    })
+      .then(res => {
+        if (res.didCancel) {
+          return;
+        }
+        setImagePicked(res['assets'][0]);
+      })
+      .catch(error => {
+        return;
+      });
+  };
+
   return (
     <KeyboardAvoidingView
       style={{flex: 1}}
@@ -33,10 +53,12 @@ const PostContentScreen = ({navigation}) => {
                 styles.postButton,
                 {
                   backgroundColor:
-                    textContent.trim() === '' ? colors.dimBlue : colors.blue,
+                    textContent.trim() === '' && !imagePicked
+                      ? colors.dimBlue
+                      : colors.blue,
                 },
               ]}
-              disabled={textContent.trim() === ''}>
+              disabled={textContent.trim() === '' && !imagePicked}>
               <Text style={styles.postText}>POST</Text>
             </TouchableOpacity>
           </View>
@@ -62,12 +84,26 @@ const PostContentScreen = ({navigation}) => {
       </TouchableWithoutFeedback>
       <View style={styles.footer}>
         <View style={styles.uploadImageContainer}>
-          <TouchableOpacity style={styles.imagePicker} activeOpacity={0.8}>
+          <TouchableOpacity
+            style={styles.imagePicker}
+            activeOpacity={0.8}
+            onPress={pickImage}>
             <CameraIcon name="camera" size={45} color={colors.eelightgray} />
           </TouchableOpacity>
-          <View style={styles.imagePicker}>
-            {/* <CameraIcon name="camera" size={45} color={colors.eelightgray} />   */}
-          </View>
+          {imagePicked && (
+            <View style={styles.uploadImage}>
+              <Image
+                source={{uri: imagePicked.uri}}
+                style={styles.pickedImage}
+              />
+              <TouchableOpacity
+                onPress={() => setImagePicked(null)}
+                activeOpacity={1}
+                style={styles.crossContainer}>
+                <Text style={styles.cross}>X</Text>
+              </TouchableOpacity>
+            </View>
+          )}
         </View>
         <View
           style={[
@@ -162,6 +198,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 10,
     marginLeft: 10,
+  },
+  uploadImage: {
+    marginLeft: 10,
+  },
+  pickedImage: {
+    height: 90,
+    width: 90,
+  },
+  crossContainer: {
+    position: 'absolute',
+    right: -3,
+    top: -8,
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    opacity: 0.8,
+    backgroundColor: colors.black,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cross: {
+    color: colors.white,
+    fontWeight: 'bold',
   },
   count: {
     height: 50,
