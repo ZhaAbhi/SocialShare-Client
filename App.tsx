b441487431d998db3a8c625c8b835759fb64bdb3
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-08-13 17:18:07 +0545

    Added auth context

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AuthNavigation from './navigation/AuthNavigation';
+import AuthContext from './context/AuthContent';
+import UnAuthNavigation from './navigation/UnAuthNavigation';
 
 const App = () => {
+  const [user, setUser] = useState();
+
   return (
     <NavigationContainer>
-      <AuthNavigation />
+      <AuthContext.Provider value={{user, setUser}}>
+        {user ? <AuthNavigation /> : <UnAuthNavigation />}
+      </AuthContext.Provider>
     </NavigationContainer>
   );
 };
