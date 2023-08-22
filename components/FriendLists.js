f83f9be87514bd0ff752a334d7d5f0729f8e2a0d
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-08-22 16:20:18 +0545

    Designed the friend list component

--- components/FriendLists.js
+++ components/FriendLists.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import loadingImage from "../assets/images/loadingImage.jpeg"
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import loadingImage from '../assets/images/loadingImage.jpeg';
+import {colors} from '../utils/colors';
 
 const FriendLists = () => {
   return (
-    <View>
-      <Text>This is friends lists</Text>
+    <View style={{marginBottom: 10}}>
+      <View style={{flexDirection: 'row'}}>
+        <Image
+          source={loadingImage}
+          style={{height: 40, width: 40, borderRadius: 20}}
+        />
+        <View style={{marginLeft: 5}}>
+          <Text style={{fontSize:16, color:colors.black}}>Email</Text>
+          <Text style={{fontSize:14, color:colors.black}}>@username</Text>
+          <Text style={{fontSize:16, color:colors.black, marginTop:5}}>This is user description.</Text>
+        </View>
+        <View style={{marginLeft: 'auto', }}>
+          <TouchableOpacity style={{backgroundColor: colors.blue, paddingLeft:10, paddingRight:10, paddingTop:3, paddingBottom:3, borderRadius:20}}>
+            <Text style={{color:colors.eelightgray, fontWeight:"bold"}}>Follow</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View
+        style={{
+          borderWidth: StyleSheet.hairlineWidth,
+          borderColor: colors.lightgray,
+          marginTop: 5,
+        }}></View>
     </View>
   );
 };
--- screens/FriendSuggestionScreen.js
+++ screens/FriendSuggestionScreen.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
-import { colors } from '../utils/colors';
+import {colors} from '../utils/colors';
 import FriendLists from '../components/FriendLists';
 
 const FriendSuggestionScreen = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <View style={{padding:20}}>
-        <Text style={{fontSize:20, color:colors.black}}>Friends Suggestions</Text>
-        <View style={{marginTop:20}}>
+      <View style={{padding: 20}}>
+        <Text style={{fontSize: 20, color: colors.black}}>
+          Friends Suggestions
+        </Text>
+        <View style={{marginTop: 20}}>
           {/* Lists of all users */}
-          <FriendLists/>
+          <FriendLists />
+          <FriendLists />
+          <FriendLists />
+          <FriendLists />
+          <FriendLists />
+          <FriendLists />
+          <FriendLists />
         </View>
       </View>
     </SafeAreaView>
