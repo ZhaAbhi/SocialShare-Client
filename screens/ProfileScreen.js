8700518cddf215f409a02c5f4286b3d848241037
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-08-30 23:13:09 +0545

    Styles profile screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -6,6 +6,8 @@ import {api} from '../config/api';
 import PostCard from '../components/PostCard';
 import loadingImage from '../assets/images/loadingImage.jpeg';
 import {colors} from '../utils/colors';
+import LocationIcon from 'react-native-vector-icons/Ionicons';
+import CalendarIcon from 'react-native-vector-icons/Ionicons';
 
 const ProfileScreen = ({route, navigation}) => {
   const {userId} = route.params;
@@ -62,8 +64,79 @@ const ProfileScreen = ({route, navigation}) => {
                 </Text>
                 <Text style={{color: colors.darkgray}}>@Abhishek</Text>
                 <View>
-                    <Text>This is my bio and only display if there is content otherwise dont display</Text>
+                  <Text style={{marginTop: 10}}>
+                    This is my bio and only display if there is content
+                    otherwise dont display
+                  </Text>
+                  <View style={{flexDirection: 'row', marginTop: 10}}>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                      }}>
+                      <LocationIcon
+                        name="location"
+                        size={15}
+                        color={colors.darkgray}
+                      />
+                      <Text style={{marginLeft: 5, color: colors.darkgray}}>
+                        Nepal
+                      </Text>
                     </View>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        marginLeft: 10,
+                      }}>
+                      <CalendarIcon
+                        name="calendar"
+                        size={15}
+                        color={colors.darkgray}
+                      />
+                      <Text style={{marginLeft: 5, color: colors.darkgray}}>
+                        Joined on August 2023
+                      </Text>
+                    </View>
+                  </View>
+                  <View style={{flexDirection: 'row', marginTop: 10}}>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        marginRight: 10,
+                      }}>
+                      <Text
+                        style={{
+                          marginRight: 5,
+                          fontWeight: 'bold',
+                          color: colors.black,
+                        }}>
+                        11
+                      </Text>
+                      <Text>Followings</Text>
+                    </View>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                      }}>
+                      <Text
+                        style={{
+                          marginRight: 5,
+                          fontWeight: 'bold',
+                          color: colors.black,
+                        }}>
+                        11
+                      </Text>
+                      <Text>Followings</Text>
+                    </View>
+                  </View>
+                </View>
               </View>
             </View>
             <View style={{marginLeft: 'auto'}}>
@@ -71,7 +144,7 @@ const ProfileScreen = ({route, navigation}) => {
                 style={{
                   backgroundColor: colors.black,
                   width: 100,
-                  height:20,
+                  height: 20,
                   justifyContent: 'center',
                   alignItems: 'center',
                   borderRadius: 10,
@@ -84,20 +157,24 @@ const ProfileScreen = ({route, navigation}) => {
           </View>
         </View>
       )}
-      {userPosts && (
-        <FlatList
-          data={userPosts}
-          keyExtractor={item => item._id}
-          renderItem={({item}) => (
-            <PostCard
-              item={item}
-              onPress={() =>
-                navigation.navigate('PostDetail', {postId: item._id})
-              }
-            />
-          )}
-        />
-      )}
+
+      <View style={{marginTop:-35}}>
+        <View style={{borderWidth: 1, borderColor: colors.darkgray}}></View>
+        {userPosts && (
+          <FlatList
+            data={userPosts}
+            keyExtractor={item => item._id}
+            renderItem={({item}) => (
+              <PostCard
+                item={item}
+                onPress={() =>
+                  navigation.navigate('PostDetail', {postId: item._id})
+                }
+              />
+            )}
+          />
+        )}
+      </View>
     </View>
   );
 };
