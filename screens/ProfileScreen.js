a6a15a64f764c118b06389f8c96af80ad6cf8b9b
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-08-30 18:24:01 +0545

    Styling header of profile screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, FlatList} from 'react-native';
+import {View, Text, FlatList, Image, TouchableOpacity} from 'react-native';
 import {retrieve} from '../utils/asyncStore';
 import axios from 'axios';
 import {api} from '../config/api';
 import PostCard from '../components/PostCard';
+import loadingImage from '../assets/images/loadingImage.jpeg';
+import {colors} from '../utils/colors';
 
 const ProfileScreen = ({route, navigation}) => {
   const {userId} = route.params;
@@ -30,6 +32,58 @@ const ProfileScreen = ({route, navigation}) => {
   }, []);
   return (
     <View style={{flex: 1}}>
+      {userPosts && (
+        <View>
+          <Image
+            source={loadingImage}
+            style={{height: 100, resizeMode: 'cover'}}
+          />
+          <View style={{padding: 10, flexDirection: 'row'}}>
+            <View style={{top: -40}}>
+              <Image
+                source={loadingImage}
+                style={{
+                  height: 80,
+                  width: 80,
+                  resizeMode: 'cover',
+                  borderRadius: 40,
+                  borderWidth: 2,
+                  borderColor: colors.white,
+                }}
+              />
+              <View style={{marginLeft: 10}}>
+                <Text
+                  style={{
+                    fontWeight: 'bold',
+                    fontSize: 16,
+                    color: colors.black,
+                  }}>
+                  Abhishek
+                </Text>
+                <Text style={{color: colors.darkgray}}>@Abhishek</Text>
+                <View>
+                    <Text>This is my bio and only display if there is content otherwise dont display</Text>
+                    </View>
+              </View>
+            </View>
+            <View style={{marginLeft: 'auto'}}>
+              <TouchableOpacity
+                style={{
+                  backgroundColor: colors.black,
+                  width: 100,
+                  height:20,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: 10,
+                }}>
+                <Text style={{color: colors.eelightgray, fontWeight: 'bold'}}>
+                  Edit Profile
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      )}
       {userPosts && (
         <FlatList
           data={userPosts}
