5cc067a5f26e427548e600dcc9b239357546e4bc
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-08-31 11:56:02 +0545

    Included search bar in messages screen

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, View, TextInput, StyleSheet, FlatList} from 'react-native';
+import {colors} from '../utils/colors';
 
 const MessageScreen = () => {
   return (
     <SafeAreaView>
-      <Text>This is message screen</Text>
+      <View style={{padding: 10}}>
+        <TextInput
+          placeholder="Search Direct Messages"
+          placeholderTextColor={colors.darkgray}
+          style={{
+            height: 30,
+            borderRadius: 25,
+            paddingLeft: 10,
+            backgroundColor: colors.elightgray,
+          }}
+        />
+      </View>
+      <View style={{borderWidth: 0.5, borderColor: colors.lightgray}}></View>
+      <View style={{flex:1}}>
+
+      </View>
     </SafeAreaView>
   );
 };
